3c62f7ff468729c4fee2cf04d93fa78a7f36dfb2
arshia <arshiarezagholi1212@gmail.com>
2024-08-13 19:25:12 +0330

    added base model test

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -12,6 +12,13 @@ class BaseModelTestCase:
         instance = self.model._default_manager.create(**self.fields)
         self.assertIsNotNone(instance.pk)
 
+    def test_ordering(self):
+        instance1 = self.model._default_manager.create(**self.fields)
+        instance2 = self.model._default_manager.create(**self.fields_2)
+        instances = self.model._default_manager.all()
+
+        self.assertEqual(list(instances), [instance2, instance1])
+
 
 class SkillModelTestCase(TestCase):
     pass
