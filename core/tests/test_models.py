b63021b45ce650dff6fb49f88cccbe2522ee5a86
arshia <arshiarezagholi1212@gmail.com>
2024-08-13 19:03:28 +0330

    added basic implemention tests for all models

---
+++ core/tests/test_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from core.models import Skill, Project, JobExperience, Education, Interest, Certificate
+
+
+class SkillModelTestCase(TestCase):
+    pass
+
+
+class ProjectModelTestCase(TestCase):
+    pass
+
+
+class JobExperienceModelTestCase(TestCase):
+    pass
+
+
+class EducationModelTestCase(TestCase):
+    pass
+
+
+class InterestModelTestCase(TestCase):
+    pass
+
+
+class CertificateModelTestCase(TestCase):
+    pass
