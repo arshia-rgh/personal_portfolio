7cd987668993f666dd6a3d65e2431ee71999bd81
arshia <arshiarezagholi1212@gmail.com>
2024-08-14 13:55:44 +0330

    implement view for download cv

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django import views
 
 
 def test_view(request):
     return render(request, "core/index.html")
+
+
+class DownloadCView(views.View):
+    def get(self, request, *args, **kwargs):
+        pass
