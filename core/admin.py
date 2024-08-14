b10ca0dbdd4b70250c18994bbd194d47650ea9bb
arshia <arshiarezagholi1212@gmail.com>
2024-08-14 15:46:36 +0330

    added project admin to test the index page

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from core.models import Skill
+from core.models import Skill, Project
 
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
     pass
