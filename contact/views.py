03bcd10bbb7a226d0a521fea5596dc88b40474f6
arshia <arshiarezagholi1212@gmail.com>
2024-08-17 21:29:22 +0330

    added extra send mail if celery not working

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from django.contrib import messages
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
-from .tasks import send_email
+
 from .forms import ContactForm
+from .tasks import send_email
 
 
 class ContactView(View):
@@ -23,7 +26,18 @@ class ContactView(View):
             email = form.cleaned_data["email"]
             message = form.cleaned_data["body"]
 
-            send_email.delay(email=email, name=name, message=message)
+            try:
+                send_email.delay(email=email, name=name, message=message)
+            except Exception as e:
+                send_mail(
+                    subject="Thank you for contacting us",
+                    message=f"Hi {name},\n\nThank you for reaching out to me. I have received your message:\n\n{message}\n\nBest regards,\nArshia.rgh",
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[
+                        email,
+                    ],
+                    fail_silently=False,
+                )
 
             form.save()
             messages.success(request, "Your message has been delivered successfully!")
