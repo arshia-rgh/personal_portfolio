6d2b451a72610eaf75b2e4bd74d947f6ef1e22af
arshia <arshiarezagholi1212@gmail.com>
2024-08-17 11:47:18 +0330

    added widgets for form fields and new css file for form

--- contact/forms.py
+++ contact/forms.py
@@ -7,3 +7,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "body": forms.Textarea(attrs={"class": "form-control"}),
+        }
